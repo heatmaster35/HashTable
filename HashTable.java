e7a2dd0fa6d0cc665e8e686eaf6c0fccb2f00423
heatmaster35 <leogomez176@gmail.com>
2015-12-03 17:49:12 -0800

    Update HashTable.java

---
+++ HashTable.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import static java.lang.System.*;
+import java.io.*;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+class Hashtable
+{
+     public static void main(
